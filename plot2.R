6a2317ed415895f879f826a8e5d4a1617cb8e4f3
RandyWaterhouse <peter.uelkes@gmx.de>
2014-06-03 10:38:46 +0200

    plot 2

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# read data from CSV file:
+data <- read.csv("household_power_consumption.txt",header=TRUE,sep=";")
+
+# convert "Date" and "Time" columns to R POSIX format:
+data$Date<-strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+
+# subset data to target dates 2007/02/01 and 2007/02/02:
+data <- data[as.Date(data$Date)=="2007-02-01" | as.Date(data$Date)=="2007-02-02",]
+
+# convert "Global_active_power" to numeric:
+data$Global_active_power <- as.numeric(data$Global_active_power)
+
+# open PNG device:
+png(filename="plot2.png",width=480,height=480)
+
+# create plot:
+plot(data$Date,data$Global_active_power/500.0,type="l",xlab="",ylab="Global Active Power (kilowatts)")
+
+# close PNG device:
+dev.off()
